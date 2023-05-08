8ab94898dec5ff25aa9b72113477510dc6ec0b50
Santos1leo <leonardocorreadossantos73@gmail.com>
2023-05-08 08:50:31 -0300

    criado o projeto e as classes  Movie e genre

---
+++ SantosFlix/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SantosFlix.Models;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ SantosFlix/Models/Genre.cs
@@ -0,0 +1,7 @@
+namespace SantosFlix.Models;
+
+ public class Genre
+  {
+    public byte Id {get; set; }
+    public string Name {get; set; }    
+  }
