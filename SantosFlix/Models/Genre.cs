9806a5605f4c1257770fbbfeff73065901cc008e
Santos1Leo <leonardocorreadossantos73@gmailm.com>
2023-05-15 08:41:09 -0300

    Inserção de Anotação dados nas classes de modelo

--- SantosFlix/Models/Genre.cs
+++ SantosFlix/Models/Genre.cs
@@ -1,7 +1,14 @@
+using  System. ComponentModel.DataAnnotations;
+
 namespace SantosFlix.Models;
 
  public class Genre
   {
+    [Key] // É uma anotação para chave primaria
+    
     public byte Id {get; set; }
+    
+    [Required] // Requerido - Not Null no banco; Torna o nome obrigatorio
+    [StringLength(30)] // Tamnho maximo da pro
     public string Name {get; set; }    
   }
--- SantosFlix/Models/Movie.cs
+++ SantosFlix/Models/Movie.cs
@@ -1,14 +1,39 @@
+using  System. ComponentModel.DataAnnotations;
+using  System. ComponentModel.DataAnnotations.Schema;
+
 namespace SantosFlix.Models;
  public class Movie
  {
+     [Key]
+      
+    
      public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)] 
      public string Title {get; set; }
+
+    [Required]
+    [StringLength(100)] 
      public string OriginaTitle {get; set; }
+
+    [Required]
+    [StringLength(8000)] 
      public string Synopsis { get; set; }
+
+      [Required]
+    [Column(TypeName ="Year")] 
      public Int16 MovieYear { get; set; }
+
+      [Required]
+     
      public Int16 Duration { get; set; }
-     public byte  AgeRating { get; set; }
+
+      [Required]
+     public byte  AgeRating { get; set; } = 0;
+      [StringLength(200)] 
      public string Image { get; set; }
+     [NotMapped]
 
      public string HourDuration { get {
          return TimeSpan.FromMinutes(Duration).ToString(@"%h'h'mm'min'");
